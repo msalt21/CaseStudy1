7eee0f216e2e776a701ae2caaa8e018103495b2a
lianatilton <59817228+lianatilton@users.noreply.github.com>
2022-09-30 13:24:32 -0500

    Update LianaV1 - SortDataTraining.m

--- LianaV1 - SortDataTraining.m
+++ LianaV1 - SortDataTraining.m
@@ -13,15 +13,15 @@ division9data = CNTY_COVID((CNTY_CENSUS.DIVISION == 9),:);
 
 %TRAINING DATA
 percent_traning = 0.8;
-div1Training = division1data(randperm(size(division1data,1), percent_testing*size(division1data,1)),:);
-div2Training = division2data(randperm(size(division2data,1), percent_testing*size(division2data,1)),:);
-div3Training = division3data(randperm(size(division3data,1), percent_testing*size(division3data,1)),:);
-div4Training= division4data(randperm(size(division4data,1), percent_testing*size(division4data,1)),:);
-div5Training = division5data(randperm(size(division5data,1), percent_testing*size(division5data,1)),:);
-div6Training= division6data(randperm(size(division6data,1), percent_testing*size(division6data,1)),:);
-div7Training = division7data(randperm(size(division7data,1), percent_testing*size(division7data,1)),:);
-div8Training = division8data(randperm(size(division8data,1), percent_testing*size(division8data,1)),:);
-div9Training = division9data(randperm(size(division9data,1), percent_testing*size(division9data,1)),:);
+div1Training = division1data(randperm(size(division1data,1), percent_traning*size(division1data,1)),:);
+div2Training = division2data(randperm(size(division2data,1), percent_traning*size(division2data,1)),:);
+div3Training = division3data(randperm(size(division3data,1), percent_traning*size(division3data,1)),:);
+div4Training= division4data(randperm(size(division4data,1), percent_traning*size(division4data,1)),:);
+div5Training = division5data(randperm(size(division5data,1), percent_traning*size(division5data,1)),:);
+div6Training= division6data(randperm(size(division6data,1), percent_traning*size(division6data,1)),:);
+div7Training = division7data(randperm(size(division7data,1), percent_traning*size(division7data,1)),:);
+div8Training = division8data(randperm(size(division8data,1), percent_traning*size(division8data,1)),:);
+div9Training = division9data(randperm(size(division9data,1), percent_traning*size(division9data,1)),:);
 
 
 % plot(division1data,'*b')
